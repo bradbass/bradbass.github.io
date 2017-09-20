b6fe180c1d4280c14c952630d333222f2b33cf9e
Brad <brad.bass@hotmail.ca>
2017-09-19 23:30:27 -0400

    moved script to script file

--- Scripts.js
+++ Scripts.js
@@ -9,3 +9,32 @@ function show_image(src, width, height, alt) {
     img.height = height;
     document.body.appendChild(img);
 }
+
+function gpslookup() {
+	var messageDiv =
+            document.getElementById('message');
+        function initLocation() {
+            var geolocation = navigator.geolocation;
+            if (geolocation) {
+                try {
+                    navigator.geolocation.getCurrentPosition(
+                        successCallback,
+                        errorCallback
+                    );
+                } catch (err) {
+                    messageDiv.innerHTML = 'Error';
+                }
+            } else {
+                messageDiv.innerHTML = 'Your browser does not support geolocation.';
+            }
+        }
+        function successCallback(location) {
+            message.innerHTML = "<p>Latitude: " +
+                location.coords.latitude + "</p>";
+            message.innerHTML += "<p>Longitude: " +
+                location.coords.longitude + "</p>";
+        }
+        function errorCallback() {
+            messageDiv.innerHTML = 'There was an error looking up your position';
+        }	
+}
